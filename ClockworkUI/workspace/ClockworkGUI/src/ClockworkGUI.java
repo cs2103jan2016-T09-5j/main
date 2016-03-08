21ff3c56a7138264153025471342f29f41bf3b08
rebekahlow-jy <rebekahlow.jy@u.nus.edu>
2016-03-08 20:31:00 +0800

    Refactored methods, changed variable names and added comments to make java file more readable.

--- ClockworkUI/workspace/ClockworkGUI/src/ClockworkGUI.java
+++ ClockworkUI/workspace/ClockworkGUI/src/ClockworkGUI.java
@@ -23,113 +23,219 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ClockworkGUI extends Application {
-
+	
+	/** Application window dimensions*/
 	public static final int DEFAULT_WINDOW_WIDTH = 600;
 	public static final int DEFAULT_WINDOW_HEIGHT = 400;
 
-	public static final Text welcomeText = new Text("Welcome, Username");
-	public static final Text userCommandDisplayText = new Text("Command: ");
-	public static final Text userCommandInputText = new Text("Type Command: ");
+	/** Static messages to display*/
+	public static final Text welcomeText = new Text("Welcome to Clockwork (:");
+	public static final Text helpText = new Text("Help: ");
+	public static final Text userCommandText = new Text("Command: ");
+	public static final Text userInputText = new Text("Type Command: ");
 	
-	public static final ArrayList<String> commandList = new ArrayList<String>(
+	/** Test arrays to display */
+	public static final ArrayList<String> helpList = new ArrayList<String>(
 			Arrays.asList("Add", "Delete", "Undo, Etc"));
 	public static final ArrayList<String> taskList = new ArrayList<String>(
 			Arrays.asList("Do Meeting Notes", "Have fun with CS2103"));
-	public static final ArrayList<String> displayList = new ArrayList<String>(
+	public static final ArrayList<String> prevCommandsList = new ArrayList<String>(
 			Arrays.asList("Add 12th Jan 2016", "Delete 12, Testing", "Mark 3", "Undo"));
-	
-	//themes the user can call to change
-	public static final String customTheme = new String();
 
+	/*
+	* ===========================================
+	* Main Program
+	* ===========================================
+	*/
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-
+	
 	@Override
 	public void start(Stage window) {
 		window.setTitle("Clockwork");
-		Scene scene;
-		switch (customTheme) {
-		  case "DARK":
-		        scene = darkTheme();
-		        break;
-		  default:
-		       	scene = defaultTheme();
-		        break;
-		}
+		Scene scene = defaultScene();
 		window.setScene(scene);
 		window.show();
 	}
 	
-	public Scene darkTheme() {
-		return null;
-	}
-
-	public Scene defaultTheme(){
+	/*
+	* ===========================================
+	* Creating Basic Layout
+	* ===========================================
+	*/
+	
+	/** 
+	 * Creates a default scene to show on GUI 
+	 * 
+	 * @return defaultScene		Scene containing messages and task list 
+	 * 							to display in various regions
+	 */
+	public Scene defaultScene(){
 		BorderPane defaultLayout = new BorderPane();
-		
-		defaultLayout.setPadding(new Insets(10));
-
-		/** Formatting Bottom Region 
-		 * Takes in ArrayList<String> displayList to display commands the User has entered
-		 * To-be-implemented: Can get Text type of what user keyed into TextBox for 
-		 * integration with Logic Component
-		 */
-		GridPane userDisplayAndInputSection = addDisplayAndInputSection(displayList);
-		defaultLayout.setBottom(userDisplayAndInputSection);
-
-		/** Formatting Top Region */
-		HBox welcomeSection = addWelcomeSection(welcomeText);
-		defaultLayout.setTop(welcomeSection);
-
-		/** Formatting Left Region 
-		 * Takes in ArrayList<String> commandList to display commands available
-		 */
-		VBox commandSection = addCommandSection(commandList);
-		defaultLayout.setLeft(commandSection);
-
-		/** Formatting Center Region 
-		 * Takes in ArrayList<String> taskList to display User Tasks*/
-		VBox taskSection = addTaskSection(taskList);
-		defaultLayout.setCenter(taskSection);
-
-		/** Formatting Right Region */
-		Button btnRight = new Button("Calendar Widget Goes Here~");
-		defaultLayout.setRight(btnRight);
-
-		/** Setting Default Scene */
+		setDisplayRegions(defaultLayout);
 		Scene defaultScene = new Scene(defaultLayout, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
 		return defaultScene;
 	}
-
-
-	/** Add User Display and Input in Bottom Row
-	 *
+	
+	/** Set top, left, center, right and bottom region to display information */
+	private void setDisplayRegions(BorderPane defaultLayout) {
+		setBottomRegion(defaultLayout);
+		setTopRegion(defaultLayout);
+		setLeftRegion(defaultLayout);
+		setCenterRegion(defaultLayout);
+		setRightRegion(defaultLayout);	
+	}
+	
+	/** Set top region to display welcome text*/
+	private void setTopRegion(BorderPane defaultLayout) {
+		HBox topSection = displayWelcomeText(welcomeText);
+		defaultLayout.setTop(topSection);
+	}
+	
+	/** Set left region to display help list */
+	private void setLeftRegion(BorderPane defaultLayout) {
+		VBox leftSection = displayHelpList(helpList);
+		defaultLayout.setLeft(leftSection);
+	}
+	
+	/** Set center region to display task list */
+	private void setCenterRegion(BorderPane defaultLayout) {
+		VBox centerSection = displayTaskList(taskList);
+		defaultLayout.setCenter(centerSection);
+	}
+	
+	/** Set right region to display calendar widget [INCOMPLETE]*/
+	private void setRightRegion(BorderPane defaultLayout) {
+		Button rightSection = new Button("INSERT CALENDAR WIDGET");
+		defaultLayout.setRight(rightSection);
+	}
+	
+	/** Set bottom region to display user command and input section*/
+	private void setBottomRegion(BorderPane defaultLayout) {
+		GridPane bottomSection = setBottomSection(prevCommandsList);
+		defaultLayout.setBottom(bottomSection);
+	}
+	
+	/*
+	* ===========================================
+	* Class Methods to integrate with Logic API
+	* ===========================================
+	*/
+	
+	/** 
+	 * Display previous commands entered by user in top subsection, and take in input
+	 * in bottom subsection in the bottom section
+	 * 
+	 * @param prevCommandsList		Previous test commands entered to show top sub-section
+	 * 								in bottom section
+	 * @return bottomSection		Bottom section to display previous commands and 
+	 * 								for user to input text
 	 */
-	private GridPane addDisplayAndInputSection(ArrayList<String> displayList) {
+	private GridPane setBottomSection(ArrayList<String> prevCommandsList) {
 		GridPane grid = new GridPane();
-		grid.setHgap(5);
-		grid.setVgap(5);
-		grid.setPadding(new Insets(5, 5, 5, 5));
+		formatBottomSection(grid);
 		
-		/** display user input */
-		ObservableList<String> displayObsList = FXCollections.observableList(displayList);
-		ListView<String> displayListView = new ListView<String>(displayObsList);
-		displayListView.setItems(displayObsList);
-		displayListView.setPrefSize(600, 250);
-		displayListView.setEditable(false);
-		displayListView.setCellFactory(TextFieldListCell.forListView());
+		ListView<String> prevCommandsListView = setTopSubsection(prevCommandsList);
+		final TextField userInput = setBottomSubsection();
 		
-		/** for user to key input */
-		grid.setPadding(new Insets(10, 10, 10, 10));
-		grid.setVgap(5);
-		grid.setHgap(5);
+		grid.getChildren().add(userInput);
+		grid.getChildren().addAll(userCommandText, prevCommandsListView, userInputText);
 		
-		//defining the user input field
+		return grid;
+	}
+
+	/** 
+	 * Display welcome message in top section [Static message, for now]
+	 * 
+	 * @param welcomeText	Text to welcome user in top section
+	 * @return topSection	Top section to display welcome text
+	 */
+	public HBox displayWelcomeText(Text welcomeText) {
+		HBox topSection = new HBox();
+		
+		styleTopSection(welcomeText, topSection);
+		
+		topSection.getChildren().add(welcomeText);
+		
+		return topSection;
+	}
+	
+	/** 
+	 * Display help list in left section
+	 * 
+	 * @param helpList		Help list of commands to show in left section
+	 * @return leftSection	Left section to display help list
+	 */
+	public VBox displayHelpList(ArrayList<String> helpList) {
+		VBox leftSection = new VBox();
+		
+		ListView<String> helpListView = formatArrayList(helpList);
+		
+		styleLeftSection(leftSection, helpListView);
+		
+		leftSection.getChildren().addAll(helpText, helpListView);
+		return leftSection;
+	}
+	
+	/** 
+	 * Display task list in center section
+	 * 
+	 * @param taskList		Task list to show in center section
+	 * @return centerSection	Left section to display task list
+	 */
+	private VBox displayTaskList(ArrayList<String> taskList) {
+		VBox centerSection = new VBox();
+		
+		ListView<String> taskListView = formatArrayList(taskList);
+		
+		styleCenterSection(centerSection, taskListView);
+		
+		centerSection.getChildren().addAll(taskListView);
+		return centerSection;
+	}
+	
+	/** 
+	 * Display previous commands entered by user in top subsection in bottom section
+	 * 
+	 * @param prevCommandsList			Previous user commands for display in ArrayList
+	 * @return prevCommandsListView		Previous user commands for display in ListView
+	 */
+	private ListView<String> setTopSubsection(ArrayList<String> prevCommandsList) {
+		ListView<String> prevCommandsListView = formatArrayList(prevCommandsList);
+		prevCommandsListView.setPrefSize(600, 250);
+		prevCommandsListView.setEditable(false);
+		prevCommandsListView.setCellFactory(TextFieldListCell.forListView());
+		
+		GridPane.setConstraints(userCommandText, 0, 0); // column=0 row=0
+		GridPane.setConstraints(prevCommandsListView, 0, 1); // column=0 row=1
+		
+		return prevCommandsListView;
+	}
+	
+	/** 
+	 * Input user command in bottom subsection in bottom section
+	 * 
+	 * @return TextField		Text field for user to input commands
+	 */
+	private TextField setBottomSubsection() {
 		final TextField userInput = new TextField();
-		userInput.setPrefColumnCount(10);
-		userInput.getText();
+		userInput.getText();	
+		handleUserInput(userInput);
 		
+		GridPane.setConstraints(userInputText, 0, 2); // column=0 row=2
+		GridPane.setConstraints(userInput, 0, 3); // column=0 row=3
+		
+		return userInput;
+	}
+	
+	/** 
+	 * Handle event after enter is pressed
+	 * 
+	 * @param userInput			User input to be handled
+	 */
+	private void handleUserInput(final TextField userInput) {
 		userInput.setOnKeyPressed(new EventHandler<KeyEvent>()
 	    {
 	        @Override
@@ -146,75 +252,67 @@ public class ClockworkGUI extends Application {
 	            
 	        }
 	    });
+	}
+	
+	/** 
+	 * Set list format from ArrayList to ListView so that list can be seen on GUI
+	 * 
+	 * @param arrayList			List of ArrayList String type
+	 * @return listView			List of ListView String type
+	 */
+	private ListView<String> formatArrayList(ArrayList<String> arrayList) {
+		ObservableList<String> obsList = FXCollections.observableList(arrayList);
+		ListView<String> listView = new ListView<String>(obsList);
+		listView.setItems(obsList);
+		return listView;
+	}
+	
+	/** 
+	 * Format grid section in bottom section for displaying previous user commands 
+	 * and user input
+	 * 
+	 * @param grid			Grid in bottom section to be formatted
+	 */
+	private void formatBottomSection(GridPane grid) {
+		/** to display previous commands */
+		grid.setHgap(5);
+		grid.setVgap(5);
+		grid.setPadding(new Insets(5, 5, 5, 5));
 		
-		grid.getChildren().add(userInput);
+		/** for user to key input */
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(5);
+		grid.setHgap(5);
 		
 		grid.getRowConstraints().add(new RowConstraints(5)); // row 0 is 5 high
 		grid.getRowConstraints().add(new RowConstraints(100)); // row 0 is 100 high
 		grid.getRowConstraints().add(new RowConstraints(10)); // row 1 is 10 high
 		grid.getRowConstraints().add(new RowConstraints(30)); // row 1 is 30 high
-
-		GridPane.setConstraints(userCommandDisplayText, 0, 0); // column=0 row=0
-		GridPane.setConstraints(displayListView, 0, 1); // column=0 row=1
-		GridPane.setConstraints(userCommandInputText, 0, 2); // column=0 row=2
-		GridPane.setConstraints(userInput, 0, 3);
-
-		grid.getChildren().addAll(userCommandDisplayText, displayListView, userCommandInputText);
-
-		return grid;
 	}
 	
-	/** Add Task List in the Center */
-	private VBox addTaskSection(ArrayList<String> taskList) {
-		VBox taskBox = new VBox();
-		taskBox.setPadding(new Insets(10, 12, 15, 12));
-		taskBox.setSpacing(10);
-		taskBox.setStyle("-fx-background-color: #FFFFFF;");
-
-		ObservableList<String> taskObsList = FXCollections.observableList(taskList);
-		ListView<String> taskListView = new ListView<String>(taskObsList);
-		taskListView.setItems(taskObsList);
-
+	//STYLING SECTIONS
+	
+	/** Styling center section containing task list */
+	private void styleCenterSection(VBox centerSection, ListView<String> taskListView) {
+		centerSection.setPadding(new Insets(10, 12, 15, 12));
+		centerSection.setSpacing(10);
+		centerSection.setStyle("-fx-background-color: #FFFFFF;");
 		taskListView.setPrefSize(70, 150);
-		
-		taskBox.getChildren().addAll(taskListView);
-		
-		return taskBox;
 	}
 
-	/** Add Command List in Left Column */
-	public VBox addCommandSection(ArrayList<String> commandList) {
-		VBox commandBox = new VBox();
-		commandBox.setPadding(new Insets(5, 12, 15, 12));
-		commandBox.setSpacing(10);
-		commandBox.setStyle("-fx-background-color: #FFFFFF;");
-
-		Text helpTextHeader = new Text("Help: ");
-
-		ObservableList<String> commandObsList = FXCollections.observableList(commandList);
-		ListView<String> commandListView = new ListView<String>(commandObsList);
-		commandListView.setItems(commandObsList);
-
+	/** Styling left section containing help list */
+	private void styleLeftSection(VBox leftSection, ListView<String> commandListView) {
+		leftSection.setPadding(new Insets(5, 12, 15, 12));
+		leftSection.setSpacing(10);
+		leftSection.setStyle("-fx-background-color: #FFFFFF;");
 		commandListView.setPrefSize(70, 150);
-
-		commandBox.getChildren().addAll(helpTextHeader, commandListView);
-
-		return commandBox;
 	}
-
-	/** Add Welcome Message in Top Row */
-	public HBox addWelcomeSection(Text welcomeText) {
-		HBox welcomeBox = new HBox();
-		welcomeBox.setPadding(new Insets(10, 12, 10, 12));
-		welcomeBox.setSpacing(5);
-		welcomeBox.setStyle("-fx-background-color: #336699;");
-
-		// Ignore font style for now
-		// welcomeText.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+	
+	/** Styling top section containing welcome text */
+	private void styleTopSection(Text welcomeText, HBox topSection) {
+		topSection.setPadding(new Insets(10, 12, 10, 12));
+		topSection.setSpacing(5);
+		topSection.setStyle("-fx-background-color: #336699;");
 		welcomeText.setFill(Color.WHITE);
-		// welcomeText.setStroke(Color.web("#7080A0"));
-		welcomeBox.getChildren().add(welcomeText);
-
-		return welcomeBox;
 	}
 }
