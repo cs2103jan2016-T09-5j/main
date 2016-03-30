bcee3613e200580de96972258455e810109c3bcb
regine7bb <regine7bb@gmail.com>
2016-03-30 20:11:14 +0800

    Update SignalHandler.java

--- src/logic/SignalHandler.java
+++ src/logic/SignalHandler.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 
 public class SignalHandler {
 
-    private static final String PREFIX = "ClockWork: ";
-    private static final String COMMAND_PREFIX = "Command: ";
+    private static final String PREFIX = "ClockWork : ";
     private static  ArrayList<String> ArrListForGUI = new ArrayList<String> ();
     
     public static ArrayList<String> getArrListForGUI(){
@@ -15,23 +14,14 @@ public class SignalHandler {
     	ArrListForGUI.clear();
     }
     public static void printSignal(Signal signal) {
-    	ArrListForGUI.clear();
         assert (signal != null);
         String message = signal.toString();
         if (message.equals(Signal.EXIT_SUCCESS)) {
         	ArrListForGUI.add(PREFIX + message);
-           // System.out.println(PREFIX + message);
             System.exit(0);
         }
         if (!message.isEmpty()) {
         	ArrListForGUI.add(PREFIX + message);
-           // System.out.println(PREFIX + message);
         }
 	}
-
-    public static void printCommandPrefix() {
-    	ArrListForGUI.clear();
-    	ArrListForGUI.add(COMMAND_PREFIX);
-        //System.out.print(COMMAND_PREFIX);
-    }
 }
