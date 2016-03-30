3dd1c826898265d3f745209ed86ba463b82f4cf8
regine7bb <regine7bb@gmail.com>
2016-03-30 20:08:53 +0800

    Update ClockWork.java

--- src/logic/ClockWork.java
+++ src/logic/ClockWork.java
@@ -83,7 +83,6 @@ public class ClockWork {
 	 */
 	public static void ClockworkLogicMain(String input, ClockWork logic) {
 
-			SignalHandler.printCommandPrefix();
 			clearConsole();
 			Signal signal;
 			try {
